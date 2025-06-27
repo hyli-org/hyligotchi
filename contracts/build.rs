4eb9c54bbe53e28edcf4e20b7e61d3b304628900
maximilien-hyle <maximilien@hyle.eu>
2025-06-27 18:46:14 +0200

    Use correct version of build.rs

--- contracts/build.rs
+++ contracts/build.rs
@@ -1,17 +1,17 @@
-#[cfg(not(feature = "build"))]
-fn main() {}
-
-#[cfg(feature = "build")]
 use sp1_helper::{build_program_with_args, BuildArgs};
-#[cfg(feature = "build")]
-fn main() {
-    build_program_with_args(
-        "./hyligotchi",
-        BuildArgs {
-            features: vec!["sp1".to_string()],
-            output_directory: Some("../elf".to_string()),
 
-            ..Default::default()
-        },
-    )
+fn main() {
+    if std::env::var("CARGO_FEATURE_BUILD").is_ok() {
+        println!("cargo:warning=Building SP1 program with build feature enabled");
+        build_program_with_args(
+            "./hyligotchi",
+            BuildArgs {
+                features: vec!["sp1".to_string()],
+                output_directory: Some("../elf".to_string()),
+                ..Default::default()
+            },
+        );
+    } else {
+        println!("cargo:warning=Skipping SP1 program build - 'build' feature not enabled");
+    }
 }
